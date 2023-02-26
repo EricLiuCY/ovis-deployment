3fb05331f286e8f27893d3e61acb115ebbde2b9d
EricLiuCY <ericliu7722@gmail.com>
2023-02-26 03:03:04 -0800

    sanity image

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,27 +1,26 @@
-import axios from 'axios'
 import { NextPageContext } from 'next'
+import { useNextSanityImage } from 'next-sanity-image'
+import Image from 'next/image'
 import styled from 'styled-components'
 import { Homepage } from '../../../types'
 import fetchHomePage from '../../GROQ/quries/fetchHomepage'
-import { useNextSanityImage } from 'next-sanity-image'
+import { withAssetFileName } from '../../utils/sanity'
 import sanityClient from '../../utils/sanity/client'
-import Image from 'next/image'
-import { withAssetFileName } from "@utils/Sanity/images"
 
 const StyledP = styled.p`
   color: red;
 `
 
 export default function Team(props: { data: Homepage}) {
-  //@ts-ignore
-  const imageProps = useNextSanityImage(sanityClient, props.data.jacklinHouseImage)
+  // @ts-ignore
+  const imageProps : any = useNextSanityImage(sanityClient, props.data.jacklinHouseImage)
   return (
     <>
       <StyledP>{props.data.valueProp}</StyledP>
       <StyledP>{props.data.valueProp}</StyledP>
       <StyledP>{props.data.valueProp}</StyledP>
-      <StyledP>{props.data.coreValues[0].name}</StyledP>
-      {/* <Image {...imageProps} loader={withAssetFileName} layout='responsive'/> */}
+      <StyledP>{props.data.coreValues[0].name}</StyledP>\
+      <Image {...imageProps} loader={withAssetFileName} layout='responsive' alt='123'/>
     </>
   )
 }
