7d113af8d821d860ff89453eee2e10637fdab2d4
EricLiuCY <ericliu7722@gmail.com>
2023-02-26 02:28:33 -0800

    WIP

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -3,18 +3,25 @@ import { NextPageContext } from 'next'
 import styled from 'styled-components'
 import { Homepage } from '../../../types'
 import fetchHomePage from '../../GROQ/quries/fetchHomepage'
+import { useNextSanityImage } from 'next-sanity-image'
+import sanityClient from '../../utils/sanity/client'
+import Image from 'next/image'
+import { withAssetFileName } from "@utils/Sanity/images"
 
 const StyledP = styled.p`
   color: red;
 `
 
 export default function Team(props: { data: Homepage}) {
+  //@ts-ignore
+  const imageProps = useNextSanityImage(sanityClient, props.data.jacklinHouseImage)
   return (
     <>
       <StyledP>{props.data.valueProp}</StyledP>
       <StyledP>{props.data.valueProp}</StyledP>
       <StyledP>{props.data.valueProp}</StyledP>
       <StyledP>{props.data.coreValues[0].name}</StyledP>
+      {/* <Image {...imageProps} loader={withAssetFileName} layout='responsive'/> */}
     </>
   )
 }
